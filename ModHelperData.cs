903f88f10320e230a815c1994d00962db9aed2f3
MrGoopyDrawers <107856947+MrGoopyDrawers@users.noreply.github.com>
2023-06-08 09:14:45 -0400

    Update ModHelperData.cs

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace BuckleMyShoes;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.1";
-    public const string Version = "1.0.0";
+    public const string Version = "1.0.1";
     public const string Name = "BuckleMyShoes";
 
     public const string Description = "Buckle My Shoes tower from the meme.";
